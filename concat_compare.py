5858e45a2ae742081dcfdb64fd39c341fd631acc
Luisnonales <202380080@psu.palawan.edu.ph>
2025-01-22 12:16:14 +0800

    concat_compare.py

--- concat_compare.py
+++ concat_compare.py
@@ -1,8 +1,8 @@
 import time
 
-start = time.time
+start = time.time()
 result = ""
-for i in range(1, 100001):
+for i in range (1, 100001):
     result += str(i)
 print("+= Time: ", time.time() - start)
 
