d960bfcdf04fe55a9830ddcb2236ab067912f53b
Luisnonales <202380080@psu.palawan.edu.ph>
2025-01-22 12:01:03 +0800

    Add files via upload

---
+++ concat_compare.py
@@ -0,0 +1,11 @@
+import time
+
+start = time.time
+result = ""
+for i in range(1, 100001):
+    result += str(i)
+print("+= Time: ", time.time() - start)
+
+start = time.time()
+result = "".join(str(i)for i in range(1, 100001,))
+print ("Using joint(): ", time.time() - start)
